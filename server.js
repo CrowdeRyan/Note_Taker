47d3fdc5c1751094c1695e986bdc3a20adcdce0d
Ryan Crowder <crowderyan@gmail.com>
2021-07-15 16:00:31 -0400

    set html route

--- server.js
+++ server.js
@@ -11,3 +11,8 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+// html routes
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "notes.html"));
+});
