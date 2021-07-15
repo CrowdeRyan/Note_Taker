9d79b52643d64bcd295bd765ea56362986d97c30
Ryan Crowder <crowderyan@gmail.com>
2021-07-15 15:59:09 -0400

    defined use

--- server.js
+++ server.js
@@ -7,3 +7,7 @@ const fs = require("fs");
 const app = express();
 
 const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
