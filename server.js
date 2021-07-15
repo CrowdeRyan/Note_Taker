bd33f9f9bf101bce5ee0d566e8ad7874d059473b
Ryan Crowder <crowderyan@gmail.com>
2021-07-15 15:56:55 -0400

    added dependencies

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require("express");
+// experimenting with this a little, Let's see how it goes
+const { v1: uuidv1 } = require("uuid");
+const path = require("path");
+const fs = require("fs");
