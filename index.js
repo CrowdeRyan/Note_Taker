a9adda5c325f6a5f8c01919f0b4b689dc81307a0
Ryan Crowder <crowderyan@gmail.com>
2021-07-16 11:50:44 -0400

    added variables

---
+++ index.js
@@ -0,0 +1,5 @@
+const $noteTitle = $(".note-title");
+const $noteText = $(".note-textarea");
+const $saveNoteBtn = $(".save-note");
+const $newNoteBtn = $(".new-note");
+const $noteList = $(".list-container .list-group");
