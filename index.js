1d01a6a38576233cc3247abc86fd0019444668ef
Ryan Crowder <crowderyan@gmail.com>
2021-07-16 11:53:05 -0400

    ajax function to pull notes

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ const $noteList = $(".list-container .list-group");
 
 // activeNote is the textarea
 let activeNote = {};
+
+// A function for pulling all notes from the db
+const getNotes = () => {
+  return $.ajax({
+    url: "/api/notes",
+    method: "GET",
+  });
+};
