4465e0fcd988d79d8d9d077633986e97b086f682
Ryan Crowder <crowderyan@gmail.com>
2021-07-16 11:52:16 -0400

    placeholder array

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ const $noteText = $(".note-textarea");
 const $saveNoteBtn = $(".save-note");
 const $newNoteBtn = $(".new-note");
 const $noteList = $(".list-container .list-group");
+
+// activeNote is the textarea
+let activeNote = {};
