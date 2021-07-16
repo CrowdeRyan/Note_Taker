4bdd7157fd9aad986113e6cd688dd665d1fc6b42
Ryan Crowder <crowderyan@gmail.com>
2021-07-16 12:03:44 -0400

    listing note titles

--- index.js
+++ index.js
@@ -106,4 +106,17 @@ const renderNoteList = (notes) => {
   $noteList.empty();
 
   const noteListItems = [];
+  const create$li = (text, withDeleteButton = true) => {
+    const $li = $("<li class='list-group-item'>");
+    const $span = $("<span>").text(text);
+    $li.append($span);
+
+    if (withDeleteButton) {
+      const $delBtn = $(
+        "<i class='fas fa-trash-alt float-right text-danger delete-note'>"
+      );
+      $li.append($delBtn);
+    }
+    return $li;
+  };
 };
